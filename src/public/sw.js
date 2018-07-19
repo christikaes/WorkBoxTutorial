83386297069aabd7125c9cdbffaf0509e74922b0
christikaes <christikaes@gmail.com>
2018-07-19 09:12:16 -0400

    Part3-CacheImage

--- src/public/sw.js
+++ src/public/sw.js
@@ -2,6 +2,13 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
+
+// Cache our images
+workbox.routing.registerRoute(
+    new RegExp('.*\.r2d2.jpg'),
+    workbox.strategies.networkFirst()
+);
