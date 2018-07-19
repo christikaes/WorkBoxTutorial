4fd98ce40f82a929119226f1aaf38eaf6496172d
christikaes <christikaes@gmail.com>
2018-07-19 09:30:57 -0400

    Part4-CacheAllRoutes

--- src/public/sw.js
+++ src/public/sw.js
@@ -7,8 +7,42 @@ if (workbox) {
     console.log(`Boo! Workbox didn't load 😬`);
 }
 
-// Cache our images
 workbox.routing.registerRoute(
-    new RegExp('.*\.r2d2.jpg'),
+    // Cache HTML files
+    new RegExp('.*\.html'),
     workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+    // Cache JS files
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+    // Cache CSS files
+    /.*\.css/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'css-cache',
+    })
+);
+
+workbox.routing.registerRoute(
+    // Cache static files
+    /.*\.(?:|jpg|mp3)/,
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+        // Use a custom cache name
+        cacheName: 'static-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Cache only 40 static items
+                maxEntries: 40,
+                // Cache for a maximum of a week
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            })
+        ],
+    })
 );
